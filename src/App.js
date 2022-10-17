1c726e6e089cde9186d3f69c567be72fc5454fdf
Sanket <msanket450@gmail.com>
2022-10-17 23:28:43 +0530

    main components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
+import { About, Header, Footer, Skills, Testimonial, Work } from "./container";
+import { Navbar } from "./components";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Navbar />
+      <Header />
+      <About />
+      <Skills />
+      <Work />
+      <Testimonial />
+      <Footer />
     </div>
   );
 }
