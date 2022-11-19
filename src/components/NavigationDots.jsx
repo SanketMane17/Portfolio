ff000349490242438876bcef19459d836515203f
Sanket <msanket450@gmail.com>
2022-11-19 17:52:12 +0530

    changing dots UI

--- src/components/NavigationDots.jsx
+++ src/components/NavigationDots.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 
 const NavigationDots = ({ active }) => (
     <div className="app__navigation">
-        {['home', 'about', 'projects', 'skills', 'testimonial', 'contact'].map((item, index) => (
+        {['home', 'about', 'projects', 'skills', 'contact'].map((item, index) => (
             <a
                 href={`#${item}`}
                 key={item + index}
